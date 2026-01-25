50bfe3485fbe3f99fec51a688b1478343912fe0a
ahmed-elnashar <a7medelnashar@email.com>
2026-01-25 14:57:38 +0200

    add recipes api functions

---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+import { env } from '../config/env'
+import type {
+    GetRecipeDetailResponse,
+    SearchRecipesResponse,
+} from '../types/search.ts'
+
+export async function searchRecipes(
+    query: string
+): Promise<SearchRecipesResponse> {
+    try {
+        const url = `${env.recipesApiUrl}?search=${query}&key=${env.apiKey}`
+        const response = await fetch(url, { method: 'GET' })
+        return (await response.json()) as SearchRecipesResponse
+    } catch (e) {
+        console.log('Error searching recipes: ', e)
+        throw e
+    }
+}
+
+export async function getRecipeByID(
+    id: string
+): Promise<GetRecipeDetailResponse> {
+    try {
+        const url = `${env.recipesApiUrl}/${id}?key=${env.apiKey}`
+        const response = await fetch(url, { method: 'GET' })
+        return (await response.json()) as GetRecipeDetailResponse
+    } catch (e) {
+        console.log('Error getting a recipe details by id: ', e)
+        throw e
+    }
+}
